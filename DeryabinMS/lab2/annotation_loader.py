5c45d6a4e34a76e2d01eff423dda15074eb2687d
BrunoBucCcellati <148016107+BrunoBucCcellati@users.noreply.github.com>
2026-02-04 11:10:43 +0300

    Update annotation_loader.py

--- DeryabinMS/lab2/annotation_loader.py
+++ DeryabinMS/lab2/annotation_loader.py
@@ -12,8 +12,9 @@ class AnnotationLoader:
 
     def __init__(self, annotation_path: str, target_class: str = "car"):
         self.annotation_path = annotation_path
-        self.target_class = target_class.lower()
-        self._annotations: Dict[str, List[Tuple[int, int, int, int]]] = self._load()
+        self.target_class = target_class
+        self._annotations: Dict[str, List[Tuple[int, int, int,
+                                                int]]] = self._load()
 
     def _load(self) -> Dict[str, List[Tuple[int, int, int, int]]]:
         annotations: Dict[str, List[Tuple[int, int, int, int]]] = {}
@@ -21,18 +22,14 @@ class AnnotationLoader:
         with open(self.annotation_path, "r", encoding="utf-8") as f:
             for line in f:
                 parts = line.strip().split()
-                if len(parts) < 6:
-                    continue
 
-                image_id_raw = parts[0]
+                image_id = parts[0].zfill(6)
                 class_name = parts[1].lower()
 
                 if class_name != self.target_class:
                     continue
 
                 x1, y1, x2, y2 = map(int, parts[2:6])
-
-                image_id = image_id_raw.zfill(6)
 
                 if image_id not in annotations:
                     annotations[image_id] = []
@@ -41,6 +38,7 @@ class AnnotationLoader:
 
         return annotations
 
-    def get_ground_truth(self, image_id: str) -> List[Tuple[int, int, int, int]]:
+    def get_ground_truth(self,
+                         image_id: str) -> List[Tuple[int, int, int, int]]:
         """Возвращаем список GT-боксов кадра"""
         return self._annotations.get(image_id, [])
